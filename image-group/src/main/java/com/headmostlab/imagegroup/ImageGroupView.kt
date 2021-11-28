6fc6dbc43399ae9b7128959a0f82db7f12422a22
vkharapaev <vkharapaev@gmail.com>
2021-11-28 18:18:39 +0300

    Version 2

--- image-group/src/main/java/com/headmostlab/imagegroup/ImageGroupView.kt
+++ image-group/src/main/java/com/headmostlab/imagegroup/ImageGroupView.kt
@@ -8,6 +8,7 @@ import android.graphics.drawable.Drawable
 import android.graphics.drawable.RippleDrawable
 import android.util.AttributeSet
 import android.view.Gravity
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
@@ -18,6 +19,8 @@ import androidx.core.content.res.getDimensionOrThrow
 import androidx.core.content.withStyledAttributes
 import androidx.core.view.doOnNextLayout
 import androidx.core.widget.TextViewCompat
+import com.headmostlab.imagegroup.ImageGroupView.Position.*
+import com.headmostlab.imagegroup.ImageGroupView.Size.*
 import kotlin.math.min
 import kotlin.math.roundToInt as rnd
 
@@ -45,6 +48,10 @@ class ImageGroupView @JvmOverloads constructor(
     private var overflowTextView: TextView? = null
     private var rippleColor = Color.TRANSPARENT
     private var clippingDrawable: Drawable? = null
+
+    private val parentHalfWidth get() = measuredWidth * 0.5
+    private val parentHalfHeight get() = measuredHeight * 0.5
+    private val gridHalfSize get() = gridSize * 0.5
 
     init {
         context.withStyledAttributes(attrs, R.styleable.ImageGroupView, defStyleAttr, defStyleRes) {
@@ -127,13 +134,22 @@ class ImageGroupView @JvmOverloads constructor(
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val (width, height) = measureView(widthMeasureSpec, heightMeasureSpec)
+        setMeasuredDimension(width.rnd(), height.rnd())
+        measureChildren()
+    }
 
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        layoutChildren()
+    }
+
+    private fun measureView(widthMeasureSpec: Int, heightMeasureSpec: Int): Pair<Double, Double> {
         val specWidth = MeasureSpec.getSize(widthMeasureSpec).toDouble()
         val specWidthMode = MeasureSpec.getMode(widthMeasureSpec)
         val specHeight = MeasureSpec.getSize(heightMeasureSpec).toDouble()
         val specHeightMode = MeasureSpec.getMode(heightMeasureSpec)
 
-        val (width, height) = when {
+        return when {
             specWidthMode == MeasureSpec.UNSPECIFIED && specHeightMode == MeasureSpec.UNSPECIFIED -> {
                 val density = resources.displayMetrics.density
                 UNSPECIFIED_WIDTH_IN_DP * density to UNSPECIFIED_HEIGHT_IN_DP * density
@@ -148,126 +164,123 @@ class ImageGroupView @JvmOverloads constructor(
                 specWidth to specHeight
             }
         }
+    }
 
-        setMeasuredDimension(width.rnd(), height.rnd())
-
-        val parentHalfWidth = measuredWidth * 0.5
-        val parentHalfHeight = measuredHeight * 0.5
-        val gridHalfSize = gridSize * 0.5
-
+    private fun doLayout(isMeasuring: Boolean = false) {
         when (images.size) {
             0 -> {}
-            1 -> measure1()
-            2 -> measure2(parentHalfWidth, gridHalfSize)
-            3 -> measure3(parentHalfWidth, parentHalfHeight, gridHalfSize)
-            4 -> measure4(parentHalfWidth, parentHalfHeight, gridHalfSize)
-            else -> {
-                measure4(parentHalfWidth, parentHalfHeight, gridHalfSize)
+            1 -> if (isMeasuring) measure1() else layout1()
+            2 -> if (isMeasuring) measure2() else layout2()
+            3 -> if (isMeasuring) measure3() else layout3()
+            4 -> if (isMeasuring) measure4() else layout4()
+            else -> if (isMeasuring) {
+                measure4()
                 measureText()
-            }
-        }
-    }
-
-    private fun measure1() {
-        imageViewList[0].measure(
-            MeasureSpec.makeMeasureSpec(measuredWidth, MeasureSpec.EXACTLY),
-            MeasureSpec.makeMeasureSpec(measuredHeight, MeasureSpec.EXACTLY)
-        )
-    }
-
-    private fun measure2(w: Double, g: Double) {
-        val width = MeasureSpec.makeMeasureSpec((w - g).rnd(), MeasureSpec.EXACTLY)
-        val height = MeasureSpec.makeMeasureSpec(measuredHeight, MeasureSpec.EXACTLY)
-        imageViewList.forEach { it.measure(width, height) }
-    }
-
-    private fun measure3(w: Double, h: Double, g: Double) {
-        val width = MeasureSpec.makeMeasureSpec((w - g).rnd(), MeasureSpec.EXACTLY)
-        val height1 = MeasureSpec.makeMeasureSpec(measuredHeight, MeasureSpec.EXACTLY)
-        val heightOther = MeasureSpec.makeMeasureSpec((h - g).rnd(), MeasureSpec.EXACTLY)
-        imageViewList[0].measure(width, height1)
-        (1..2).forEach { imageViewList[it].measure(width, heightOther) }
-    }
-
-    private fun measure4(w: Double, h: Double, g: Double) {
-        val width = MeasureSpec.makeMeasureSpec((w - g).rnd(), MeasureSpec.EXACTLY)
-        val height = MeasureSpec.makeMeasureSpec((h - g).rnd(), MeasureSpec.EXACTLY)
-        imageViewList.forEach { it.measure(width, height) }
-    }
-
-    private fun measureText() {
-        overflowTextView?.measure(
-            MeasureSpec.makeMeasureSpec(imageViewList[3].measuredWidth, MeasureSpec.EXACTLY),
-            MeasureSpec.makeMeasureSpec(imageViewList[3].measuredHeight, MeasureSpec.EXACTLY)
-        )
-    }
-
-    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
-        val parentHalfWidth = measuredWidth * 0.5
-        val parentHalfHeight = measuredHeight * 0.5
-        val gridHalfSize = gridSize * 0.5
-
-        when (images.size) {
-            0 -> {}
-            1 -> layout1()
-            2 -> layout2(parentHalfWidth, gridHalfSize)
-            3 -> layout3(parentHalfWidth, parentHalfHeight, gridHalfSize)
-            4 -> layout4(parentHalfWidth, parentHalfHeight, gridHalfSize)
-            else -> {
-                layout4(parentHalfWidth, parentHalfHeight, gridHalfSize)
+            } else {
+                layout4()
                 layoutText()
             }
         }
     }
 
+    private fun measureChildren() {
+        doLayout(isMeasuring = true)
+    }
+
+    private fun layoutChildren() {
+        doLayout()
+    }
+
+    private enum class Size {
+        WHOLE,
+        HALF,
+        QUARTER
+    }
+
+    private fun View.measure(size: Size) {
+        val (width, height) = when (size) {
+            WHOLE -> this@ImageGroupView.measuredWidth to this@ImageGroupView.measuredHeight
+            HALF -> (parentHalfWidth - gridHalfSize).rnd() to this@ImageGroupView.measuredHeight
+            QUARTER -> (parentHalfWidth - gridHalfSize).rnd() to (parentHalfHeight - gridHalfSize).rnd()
+        }
+        measure(
+            MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY),
+            MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY)
+        )
+    }
+
+    private fun measure1() {
+        imageViewList[0].measure(WHOLE)
+    }
+
+    private fun measure2() {
+        imageViewList[0].measure(HALF)
+        imageViewList[1].measure(HALF)
+    }
+
+    private fun measure3() {
+        imageViewList[0].measure(HALF)
+        imageViewList[1].measure(QUARTER)
+        imageViewList[2].measure(QUARTER)
+    }
+
+    private fun measure4() {
+        imageViewList.forEach { it.measure(QUARTER) }
+    }
+
+    private fun measureText() {
+        overflowTextView?.measure(QUARTER)
+    }
+
+    private enum class Position {
+        TOP_LEFT,
+        TOP_RIGHT,
+        BOTTOM_RIGHT,
+        BOTTOM_LEFT
+    }
+
+    private fun View.layout(position: Position) {
+        when (position) {
+            TOP_LEFT -> layout(0, 0, measuredWidth, measuredHeight)
+            TOP_RIGHT -> layout(
+                (parentHalfWidth + gridHalfSize).rnd(), 0,
+                (parentHalfWidth + gridHalfSize + measuredWidth).rnd(), measuredHeight
+            )
+            BOTTOM_LEFT -> layout(
+                0, (parentHalfHeight + gridHalfSize).rnd(),
+                measuredWidth, (parentHalfHeight + gridHalfSize + measuredHeight).rnd()
+            )
+            BOTTOM_RIGHT -> layout(
+                (parentHalfWidth + gridHalfSize).rnd(),
+                (parentHalfHeight + gridHalfSize).rnd(),
+                (parentHalfWidth + gridHalfSize + measuredWidth).rnd(),
+                (parentHalfHeight + gridHalfSize + measuredHeight).rnd()
+            )
+        }
+    }
+
     private fun layout1() {
-        imageViewList[0].apply { layout(0, 0, measuredWidth, measuredHeight) }
+        imageViewList[0].layout(TOP_LEFT)
     }
 
-    private fun layout2(w: Double, g: Double) {
-        imageViewList[0].apply {
-            layout(0, 0, measuredWidth, measuredHeight)
-        }
-        imageViewList[1].apply {
-            layout((w + g).rnd(), 0, (w + g).rnd() + measuredWidth, measuredHeight)
-        }
+    private fun layout2() {
+        imageViewList[0].layout(TOP_LEFT)
+        imageViewList[1].layout(TOP_RIGHT)
     }
 
-    private fun layout3(w: Double, h: Double, g: Double) {
-        imageViewList[0].apply {
-            layout(0, 0, measuredWidth, measuredHeight)
-        }
-        imageViewList[1].apply {
-            layout((w + g).rnd(), 0, (w + g + measuredWidth).rnd(), measuredHeight)
-        }
-        imageViewList[2].apply {
-            layout(
-                (w + g).rnd(), (h + g).rnd(),
-                (w + g + measuredWidth).rnd(), (h + g + measuredHeight).rnd()
-            )
-        }
+    private fun layout3() {
+        layout2()
+        imageViewList[2].layout(BOTTOM_RIGHT)
     }
 
-    private fun layout4(w: Double, h: Double, g: Double) {
-        imageViewList[0].apply {
-            layout(0, 0, measuredWidth, measuredHeight)
-        }
-        imageViewList[1].apply {
-            layout((w + g).rnd(), 0, (w + g + measuredWidth).rnd(), measuredHeight)
-        }
-        imageViewList[2].apply {
-            layout(0, (h + g).rnd(), measuredWidth, (h + g + measuredHeight).rnd())
-        }
-        imageViewList[3].apply {
-            layout(
-                (w + g).rnd(), (h + g).rnd(),
-                (w + g + measuredWidth).rnd(), (h + g + measuredHeight).rnd()
-            )
-        }
+    private fun layout4() {
+        layout2()
+        imageViewList[2].layout(BOTTOM_LEFT)
+        imageViewList[3].layout(BOTTOM_RIGHT)
     }
 
     private fun layoutText() {
-        imageViewList[3].run { overflowTextView?.layout(left, top, right, bottom) }
+        overflowTextView?.layout(BOTTOM_RIGHT)
     }
 
     private class RippleImageView(context: Context, @ColorInt private val color: Int) :
